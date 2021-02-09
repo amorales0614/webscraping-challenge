197daf7184e69a515982ac60aa1a5cfe9840cfaf
Ana Morales <ammorales614@gmail.com>
2021-02-09 16:48:44 -0500

    commit 17

--- mars_flask.py
+++ mars_flask.py
@@ -5,23 +5,21 @@ import scrape_mars
 
 app = Flask(__name__)
 
-app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
-mongo = PyMongo(app)
+mongo = PyMongo(app, uri='mongodb://localhost:27017/mars_app')
 
 @app.route("/")
 def home():
 
-    mars_dict = mongo.db.mars_dict.find_one()
-    return render_template("index.html", mars=mars_dict)
+    home_data = mongo.db.mars_dict.find_one()
+    return render_template("index.html", mars=home_data)
 
 
 @app.route("/scrape")
 def scrape():
   
-    mars_dict = mongo.db.mars_dict
     mars_data = scrape_mars.scrape()
     mars_dict.update({}, mars_data, upsert=True)
-    return redirect("/", code=302)
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run(debug=True)
