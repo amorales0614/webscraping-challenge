7cf7b6305def9f6e455629f06f20e9e01a6c577e
Ana Morales <ammorales614@gmail.com>
2021-02-09 16:34:38 -0500

    commit 16

--- scrape_mars.py
+++ scrape_mars.py
@@ -7,7 +7,7 @@ import pymongo
 
 def init_browser():
     executable_path = {'executable_path': 'C:/webdrivers/chromedriver.exe'}
-	browser = Browser('chrome', **executable_path, headless=False)
+    browser = Browser('chrome', **executable_path, headless=False)
 
 def scrape():
     browser = init_browser()
@@ -15,72 +15,72 @@ def scrape():
     #latest news article
     url = 'https://mars.nasa.gov/news/'
 
-	browser.visit(url)
+    browser.visit(url)
 
-	html = browser.html
+    html = browser.html
 
-	latest_soup = BeautifulSoup(html, 'html.parser')
+    latest_soup = BeautifulSoup(html, 'html.parser')
 
-	news_title = latest_soup.find_all('div', class_='content_title')[1].text
-	news_p = latest_soup.find_all('div', class_='article_teaser_body')[0].text
+    news_title = latest_soup.find_all('div', class_='content_title')[1].text
+    news_p = latest_soup.find_all('div', class_='article_teaser_body')[0].text
 
-	#featured image
-	fimage_url = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
+    #featured image
+    fimage_url = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
 
-	browser.visit(fimage_url)
+    browser.visit(fimage_url)
 
-	image_html = browser.html
+    image_html = browser.html
 
-	image_soup = BeautifulSoup(image_html, 'html.parser')
+    image_soup = BeautifulSoup(image_html, 'html.parser')
 
-	feat_image = image_soup.find('div', class_='floating_text_area')
+    feat_image = image_soup.find('div', class_='floating_text_area')
 
-	image_element = feat_image.a['href']
+    image_element = feat_image.a['href']
 
-	featured_image_url = f'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/{image_element}'
+    featured_image_url = f'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/{image_element}'
 
-	#mars fact table
-	table_url = 'https://space-facts.com/mars/'
-	url_display = pd.read_html(table_url)[0]
-	table_html = url_display.to_html(classes='table table-striped')
+    #mars fact table
+    table_url = 'https://space-facts.com/mars/'
+    url_display = pd.read_html(table_url)[0]
+    table_html = url_display.to_html(classes='table table-striped')
 
-	#hemispheres
+    #hemispheres
 
-	hemi_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    hemi_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
 
-	browser.visit(hemi_url)
+    browser.visit(hemi_url)
 
-	hemi_h = browser.html
+    hemi_h = browser.html
 
-	hemi_soup = BeautifulSoup(hemi_h, 'html.parser')
+    hemi_soup = BeautifulSoup(hemi_h, 'html.parser')
 
-	hemi_results = hemi_soup.find('div', class_='collapsible results')
+    hemi_results = hemi_soup.find('div', class_='collapsible results')
 
-	hemi_results_sub = hemi_results.find_all('div', class_='item')
+    hemi_results_sub = hemi_results.find_all('div', class_='item')
 
-		hemi_links = []
+    hemi_links = []
 
-	for x in hemi_results_sub:
-	    hemi_desc = x.find('div', class_='description')
-	    hemi_title = hemi_desc.h3.text
-	    
-	    h_link = hemi_desc.a['href']
-	    browser.visit(f'https://astrogeology.usgs.gov/{h_link}')
-	    
-	    fh_html = browser.html
-	    fh_soup = BeautifulSoup(fh_html, 'html.parser')
-	    
-	    fh_link = fh_soup.find('div', class_='downloads')
-	    fh_url = fh_link.find('li').a['href']
-	    
-	    #adding to dictionary
-	    hemi_dict = {}
-	    hemi_dict['Title'] = hemi_title
-	    hemi_dict['Link'] = fh_url
-	    
-	    hemi_links.append(hemi_dict)
+    for x in hemi_results_sub:
+        hemi_desc = x.find('div', class_='description')
+        hemi_title = hemi_desc.h3.text
+        
+        h_link = hemi_desc.a['href']
+        browser.visit(f'https://astrogeology.usgs.gov/{h_link}')
+        
+        fh_html = browser.html
+        fh_soup = BeautifulSoup(fh_html, 'html.parser')
+        
+        fh_link = fh_soup.find('div', class_='downloads')
+        fh_url = fh_link.find('li').a['href']
+        
+        #adding to dictionary
+        hemi_dict = {}
+        hemi_dict['Title'] = hemi_title
+        hemi_dict['Link'] = fh_url
+        
+        hemi_links.append(hemi_dict)
 
-	mars_dict = {
+    mars_dict = {
         "news_title": news_title,
         "news_p": news_p,
         "featured_image_url": featured_image_url,
@@ -88,4 +88,4 @@ def scrape():
         "hemisphere_images": hemi_links
     }
 
-return mars_dict
+    return mars_dict
